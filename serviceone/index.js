01091827606d9b61e66db7e1d9a947145f51008c
sid34protectonce <siddharth.s@protectonce.com>
2023-08-15 15:57:31 +0530

    update logs

--- serviceone/index.js
+++ serviceone/index.js
@@ -1,9 +1,37 @@
-module.exports.handler = async (event) => {
+module.exports.handler1 = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Hello from service one!', 
+        message: 'Hello from service one!\nThis is handler1', 
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.handler2 = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Hello from service one!\nThis is handler2",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.handler3 = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Hello from service one!\nThis is handler3",
         input: event,
       },
       null,
